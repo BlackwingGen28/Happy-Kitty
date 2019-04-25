f33addbf2cefe760e4d8f6165d2f48050a1d5205
Jordan Morgan <jordan.morgan2@smail.astate.edu>
2019-04-25 14:56:58 -0500

    kitty now has a happiness and the player has basic actions to make kitty happy/mad.

--- Happy Kitty/ViewController.swift
+++ Happy Kitty/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var happyKitty: UIImageView!
+    @IBOutlet weak var neutralKitty: UIImageView!
+    @IBOutlet weak var angryKitty: UIImageView!
+    
+    var kittyHappiness: Int = 50;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initialize_kitty()
     }
 
-
+    func initialize_kitty () {
+        happyKitty.alpha = 0
+        neutralKitty.alpha = 1
+        angryKitty.alpha = 0
+    }
+    
+    func check_kitty_happiness () {
+        if (kittyHappiness > 75) {
+            happyKitty.alpha = 1
+            neutralKitty.alpha = 0
+            angryKitty.alpha = 0
+        } else if (kittyHappiness < 25) {
+            happyKitty.alpha = 0
+            neutralKitty.alpha = 0
+            angryKitty.alpha = 1
+        } else {
+            happyKitty.alpha = 0
+            neutralKitty.alpha = 1
+            angryKitty.alpha = 0
+        }
+        
+    }
+    
+    @IBAction func playButtonPressed(_ sender: Any) {
+        kittyHappiness += 5
+        check_kitty_happiness()
+    }
+    
+    @IBAction func sprayButtonPressed(_ sender: Any) {
+        kittyHappiness -= 5
+        check_kitty_happiness()
+    }
+    
+    @IBAction func feedButtonPressed(_ sender: Any) {
+        kittyHappiness += 10
+        check_kitty_happiness()
+    }
 }
 
